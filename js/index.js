0a1d3b67015397e0899b5522b1cabaf310281b56
TLintott <traceyjlintott@hotmail.co.uk>
2023-08-18 14:12:56 +0100

    time code modified. Search engine code completed

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 //Day & Time
-function showDayTime() {
-  let now = new Date();
-  let hours = now.getHours();
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
-  let minutes = now.getMinutes();
+  let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
@@ -17,41 +17,53 @@ function showDayTime() {
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday"
+    "Saturday",
   ];
-  let day = days[now.getDay()];
-
-  let dayTime = document.querySelector("#dayTime");
-  date.innerHTML = day + " " + hours + ":" + minutes;
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
-
-window.addEventListener("DOMContentLoaded", showDayTime);
 
 //Search Engine 
 
 function findTemperature(response){
- let temperature =document.querySelector("#temporature");
+ let temporature =document.querySelector("#temporature");
  let city = document.querySelector("#city");
  let description = document.querySelector("#description");
  let humidity = document.querySelector("#humidity");
  let wind = document.querySelector("#wind");
+ let date = document.querySelector("#date");
  let icon = document.querySelector("#icon");
 
- let celsiusTemperature = response.data.main.temp;
-
-
- 
- temperature.innerHTML= Math.round(celsiusTemperature);
- city.innerHTML = response.data.name
-
-
-
-
-}
-
-function showTemperature (response){
+ let celsiusTemperature = response.data.temperature.current;
 console.log(response.data);
+ date.innerHTML = formatDate(response.data.time * 1000);
+ temp.innerHTML= Math.round(celsiusTemperature);
+ city.innerHTML = response.data.city;
+ description.innerHTML = response.data.condition.description;
+ humidity.innerHTML =response.data.temperature.humidity;
+ wind.innerHTML= Math.round(response.data.wind.speed);
+ icon.setAttribute(
+  "src", `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+ );
 }
+
+
+
+function search(city){
 let apiKey = "4b123t2611cd046f6e3o15d4a0230eef";
-let apiUrl = "https://api.shecodes.io/weather/v1/current?query=${city}&key=4b123t2611cd046f6e3o15d4a0230eef&units=metric";
-axios.get(apiUrl).then(showTemperature);
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=4b123t2611cd046f6e3o15d4a0230eef&units=metric`;
+axios.get(apiUrl).then(findTemperature);
+
+}
+
+
+function handleSubmit (event) {
+  event.preventDefault();
+  let cityinput = document.querySelector("#city-input");
+  search (cityinput.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("London");
